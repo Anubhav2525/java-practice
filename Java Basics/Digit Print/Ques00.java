91623fbcdeefa2a81009126474c25f2d3af1acb1
Anubhav Ranjan <anbhavranjan2525@gmail.com>
2023-09-26 18:07:31 +0530

    update

--- Java Basics/Digit Print/Ques00.java
+++ Java Basics/Digit Print/Ques00.java
@@ -1,24 +1,42 @@
 public class Ques00 {
     public static void main(String[] args) {
         for (int i = 1; i <= 2; i++) {
-            for (int j = 1; j <= 8; j++)
+            for (int j = 1; j <= 10; j++)
                 System.out.print("*");
             System.out.println();
         }
         for (int i = 1; i <= 6; i++) {
             for (int j = 1; j <= 2; j++)
                 System.out.print("*");
-            for (int sp = 1; sp <= 4; sp++)
+            for (int sp = 1; sp <= 6; sp++)
                 System.out.print(" ");
             for (int k = 1; k <= 2; k++)
                 System.out.print("*");
             System.out.println();
-        }       
-       
+        }
         for (int i = 1; i <= 2; i++) {
-            for (int j = 1; j <= 8; j++)
+            for (int j = 1; j <= 10; j++)
                 System.out.print("*");
             System.out.println();
         }
     }
 }
+
+
+
+/* 
+
+Output -
+
+**********
+**********
+**      **
+**      **
+**      **
+**      **
+**      **
+**      **
+**********
+**********
+
+*/
